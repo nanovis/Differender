2192a2c02b2d5bb64f70bc025baa7635a0f9882d
feng liang <feng.liang@kaust.edu.sa>
2021-05-17 00:10:59 +0300

    finished basic slicing, super slow, added TODO

--- src/hello.py
+++ src/hello.py
@@ -1,30 +1,36 @@
 import taichi as ti
+import utils
+import numpy as np
 
 ti.init(arch=ti.cuda)
-n = 320
-pixels = ti.field(dtype=float, shape=(n * 2, n))
+# TODO: check dimension issue, see data placement in np and in ti.field
+dims, data = utils.load_head_data()
+data_f = data / float(np.iinfo(np.uint16).max)
+data_f = data_f.reshape(-1)
+width, height, slice_num = dims[0], dims[1], dims[2]
+pixels = ti.field(dtype=float, shape=(width * 5, height * 5))
+data_field = ti.field(ti.f64, shape=(data_f.shape[0]))
+data_field.from_numpy(data_f)
 
 
-@ti.func  # __device__
-def complex_sqr(z):
-    return ti.Vector([z[0] ** 2 - z[1] ** 2, z[0] * z[1] * 2])
-
-
-@ti.kernel  # __global__
-def paint(t: float):  # must be type-hinted
+@ti.kernel
+def paint_slice(slice_idx: int):
     for i, j in pixels:
-        c = ti.Vector([-0.8, ti.cos(t) * 0.2])
-        z = ti.Vector([i / n - 1, j / n - 0.5])
-        iterations = 0
-        while z.norm() < 20 and iterations < 50:
-            z = complex_sqr(z) + c
-            iterations += 1
-        pixels[i, j] = 1 - iterations * 0.02
+        idx_x = i // 5
+        idx_y = j // 5
+        pixels[i, j] = data_field[idx_x + idx_y * width + slice_idx * (width * height)]
 
 
-gui = ti.GUI("Julia Set", res=(n * 2, n))
-
-for i in range(600):
-    paint(i * 0.03)
+gui = ti.GUI("SciVis Slicing", res=(width * 5, height * 5))
+slice_num = int(slice_num)
+slice_idx = 3
+count = 0
+while gui.running:
+    paint_slice(slice_idx)
     gui.set_image(pixels)
     gui.show()
+    count += 1
+    if count % 60 == 0:
+        count = 0
+        slice_idx += 1
+        slice_idx = slice_idx % slice_num
