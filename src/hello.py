23b110ebd234a226215f32d06b9d756742005070
feng liang <feng.liang@kaust.edu.sa>
2021-05-17 23:23:05 +0300

    fixed reshape issue in np, added pickled data the reshaping scheme is not the same as CG representation

--- src/hello.py
+++ src/hello.py
@@ -3,25 +3,24 @@ import utils
 import numpy as np
 
 ti.init(arch=ti.cuda)
-# TODO: check dimension issue, see data placement in np and in ti.field
-dims, data = utils.load_head_data()
+data = utils.load_head_data()
 data_f = data / float(np.iinfo(np.uint16).max)
-data_f = data_f.reshape(-1)
-width, height, slice_num = dims[0], dims[1], dims[2]
-pixels = ti.field(dtype=float, shape=(width * 5, height * 5))
-data_field = ti.field(ti.f64, shape=(data_f.shape[0]))
+width, height, slice_num = data.shape
+scaling = 5
+pixels = ti.field(dtype=float, shape=(width * scaling, height * scaling))
+data_field = ti.field(ti.f64, shape=(width, height, slice_num))
 data_field.from_numpy(data_f)
 
 
 @ti.kernel
 def paint_slice(slice_idx: int):
     for i, j in pixels:
-        idx_x = i // 5
-        idx_y = j // 5
-        pixels[i, j] = data_field[idx_x + idx_y * width + slice_idx * (width * height)]
+        idx_x = i // scaling
+        idx_y = j // scaling
+        pixels[i, j] = data_field[idx_x, idx_y, slice_idx]
 
 
-gui = ti.GUI("SciVis Slicing", res=(width * 5, height * 5))
+gui = ti.GUI("SciVis Slicing", res=(width * scaling, height * scaling))
 slice_num = int(slice_num)
 slice_idx = 3
 count = 0
--- src/utils.py
+++ src/utils.py
@@ -1,17 +1,36 @@
 import numpy as np
+import pickle
+
+
+def load_data(file_path):
+    with open(file_path, "rb") as f:
+        data = pickle.load(f)
+        return data
+
+
+def dump_data(data, file_path):
+    with open(file_path, "wb") as f:
+        pickle.dump(data, f)
 
 
 def load_head_data():
+    return load_data("./data/skewed_head.pickle")
+
+
+def _load_head_data():
     data_path = "./data/skewed_head.dat"
     dims = np.fromfile(data_path, dtype=np.uint16, count=3)
-    data = np.fromfile(data_path, dtype=np.uint16, offset=3 * 2)
-    data = data << 4
-    return dims, data.reshape(dims)
+    data = np.fromfile(data_path, dtype=np.uint16, offset=3 * 2) << 4
+    data_3d = np.zeros(dims, dtype=np.uint16)
+    for z in range(dims[2]):
+        z_idx_base = z * dims[0] * dims[1]
+        for y in range(dims[1]):
+            y_idx_base = y * dims[0]
+            for x in range(dims[0]):
+                data_3d[x, y, z] = data[x + y_idx_base + z_idx_base]
+    return dims, data_3d
 
 
 if __name__ == "__main__":
-    dims, data = load_head_data()
-    fdata = data / float(np.iinfo(np.uint16).max)
-    print(dims)
-    print(data.shape)
-    print(fdata.dtype)
+    dims, data = _load_head_data()
+    dump_data(data, "./data/skewed_head.pickle")
