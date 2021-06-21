2ecc3f96abd0443e5779c24d2db1d98813e54dce
Dominik Engel <dominik.engel@mail.de>
2021-06-21 10:52:33 +0200

    some docstrings

--- src/volume_raycaster.py
+++ src/volume_raycaster.py
@@ -13,6 +13,14 @@ import matplotlib.pyplot as plt
 
 @ti.func
 def low_high_frac(x: float):
+    ''' Returns the integer value below and above, as well as the frac
+
+    Args:
+        x (float): Floating point number
+
+    Returns:
+        int, int, float: floor, ceil, frac of `x`
+    '''
     low = tl.floor(x)
     high = low + 1.0
     frac = x - low
@@ -20,6 +28,17 @@ def low_high_frac(x: float):
 
 @ti.func
 def get_entry_exit_points(look_from, view_dir, bl, tr):
+    ''' Computes the entry and exit points of a given ray
+
+    Args:
+        look_from (tl.vec3): Camera Position as vec3
+        view_dir (tl.vec): View direction as vec3, normalized
+        bl (tl.vec3): Bottom left of the bounding box
+        tr (tl.vec3): Top right of the bounding box
+
+    Returns:
+        float, float, bool: Distance to entry, distance to exit, bool whether box is hit
+    '''
     dirfrac = 1.0 / view_dir
     t1 = (bl.x - look_from.x) * dirfrac.x
     t2 = (tr.x - look_from.x) * dirfrac.x
@@ -37,6 +56,13 @@ def get_entry_exit_points(look_from, view_dir, bl, tr):
 @ti.data_oriented
 class VolumeRaycaster():
     def __init__(self, resolution=(240, 240), fov=60.0, nearfar=(0.1, 100.0)):
+        ''' Initializes Volume Raycaster. Make sure to .set_volume() and .set_tf_tex() after initialization
+
+        Args:
+            resolution (2-tuple of int, optional): Resolution of the rendering (w,h). Defaults to (240, 240).
+            fov (float, optional): Field of view of the camera in degrees. Defaults to 60.0.
+            nearfar (2-tuple of float, optional): Near and far plane distance used for perspective projection. Defaults to (0.1, 100.0).
+        '''
         self.resolution = resolution
         self.aspect = resolution[0] / resolution[1]
         self.fov_deg = fov
@@ -53,6 +79,17 @@ class VolumeRaycaster():
 
     @ti.func
     def get_ray_direction(self, orig, view_dir, x: float, y: float):
+        ''' Compute ray direction for perspecive camera.
+
+        Args:
+            orig (tl.vec3): Camera position
+            view_dir (tl.vec3): View direction, normalized
+            x (float): Image coordinate in [0,1] along width
+            y (float): Image coordinate in [0,1] along height
+
+        Returns:
+            tl.vec3: Ray direction from camera origin to pixel specified through `x` and `y`
+        '''
         u = x - 0.5
         v = y - 0.5
 
@@ -68,6 +105,14 @@ class VolumeRaycaster():
 
     @ti.func
     def sample_volume_trilinear(self, pos):
+        ''' Samples volume data at `pos` and trilinearly interpolates the value
+
+        Args:
+            pos (tl.vec3): Position to sample the volume in [-1, 1]^3
+
+        Returns:
+            float: Sampled interpolated intensity
+        '''
         pos = (0.5*pos) + 0.5
         x_low, x_high, x_frac = low_high_frac(pos.x)
         y_low, y_high, y_frac = low_high_frac(pos.y)
@@ -92,12 +137,28 @@ class VolumeRaycaster():
 
     @ti.func
     def apply_transfer_function(self, intensity: float):
+        ''' Applies a 1D transfer function to a given intensity value
+
+        Args:
+            intensity (float): Intensity
+
+        Returns:
+            tl.vec4: Color and opacity for given `intensity`
+        '''
         length = ti.static(float(self.tf_tex.shape[0]))
         low, high, frac = low_high_frac(intensity * length)
         return tl.mix(self.tf_tex[low], self.tf_tex[high], frac)
 
     @ti.kernel
     def raycast(self, cam_pos_x: float, cam_pos_y: float, cam_pos_z: float, sampling_rate: float):
+        ''' Produce a rendering
+
+        Args:
+            cam_pos_x (float): Camera Pos x
+            cam_pos_y (float): Camera Pos y
+            cam_pos_z (float): Camera Pos z
+            sampling_rate (float): Sampling rate along the ray
+        '''
         max_x = ti.static(float(self.render.shape[0]))
         max_y = ti.static(float(self.render.shape[1]))
         vol_diag = ti.static((tl.vec3(*self.volume.shape) - tl.vec3(1.0)).norm())
@@ -117,17 +178,18 @@ class VolumeRaycaster():
                 ray_len = (tmax - tmin)
                 n_samples = ti.ceil(sampling_rate * ray_len * vol_diag) # Number of samples according to https://osf.io/u9qnz
                 t_inc = ray_len / n_samples # Increment for steps along vd
-                t = tmin + 0.5 * t_inc # Start of ray
+                t = tmin + 0.5 * t_inc # Start of ray is look_from + t * vd
                 color = tl.vec4(0.0) # Color to be composited
                 cnt = 0
-                while cnt < n_samples and color.w < 0.99:
-                    pos = look_from + t * vd
-                    intensity = self.sample_volume_trilinear(pos)
-                    s_color = self.apply_transfer_function(intensity) # Sample Color
-                    s_color = tl.vec4(s_color.xyz * s_color.w, s_color.w)
-                    color += (1.0 - color.w) * s_color
+                while cnt < n_samples and color.w < 0.99: # Early ray termination
+                    pos = look_from + t * vd # Current pos at this step
+                    intensity = self.sample_volume_trilinear(pos)     # Sample intensity
+                    s_color = self.apply_transfer_function(intensity) # Sample Color and opacity
+                    s_color = tl.vec4(s_color.xyz * s_color.w, s_color.w) # pre-multiply alpha
+                    color += (1.0 - color.w) * s_color  # Composite color
                     t += t_inc
                     cnt += 1
+                # Save result to global buffers
                 self.render[i, j] = color.xyz
                 self.opacity[i,j] = color.w
             else: # Skip empty space
@@ -141,7 +203,7 @@ if __name__ == '__main__':
     vr = VolumeRaycaster(resolution=(400,400))
     print('GUI Resolution:', window.res, '  Framebuffer shape: ', vr.render.shape)
     tfgen = TFGenerator()
-    tf = tex_from_pts(tfgen.generate(), 128).numpy()
+    tf = tex_from_pts(tfgen.generate(), 128).numpy().astype(np.float32)
     vol = (load_head_data() / float(np.iinfo(np.uint16).max)).astype(np.float32)
     vr.set_volume(vol)
     vr.set_tf_tex(tf)
