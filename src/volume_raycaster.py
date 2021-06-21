8f830013e0e5497385c799f4173972bf88ff3d0b
Dominik Engel <dominik.engel@mail.de>
2021-06-21 18:21:25 +0200

    raycaster works with phong shading

--- src/volume_raycaster.py
+++ src/volume_raycaster.py
@@ -2,14 +2,12 @@ import taichi as ti
 import taichi_glsl as tl
 
 import numpy as np
-
-from utils import load_head_data
-
-from torchvtk.utils import TFGenerator, tex_from_pts
-
 import matplotlib.pyplot as plt
 
-# Taichi global fields
+from torchvtk.utils import TFGenerator, tex_from_pts
+from torchvtk.datasets import TorchDataset
+
+from utils import load_head_data
 
 @ti.func
 def low_high_frac(x: float):
@@ -21,9 +19,10 @@ def low_high_frac(x: float):
     Returns:
         int, int, float: floor, ceil, frac of `x`
     '''
-    low = tl.floor(x)
-    high = low + 1.0
-    frac = x - low
+    x = ti.max(x, 0.0)
+    low = ti.floor(x)
+    high = low + 1
+    frac = x - float(low)
     return int(low), int(high), frac
 
 @ti.func
@@ -55,24 +54,31 @@ def get_entry_exit_points(look_from, view_dir, bl, tr):
 
 @ti.data_oriented
 class VolumeRaycaster():
-    def __init__(self, resolution=(240, 240), fov=60.0, nearfar=(0.1, 100.0)):
+    def __init__(self, volume_resolution, render_resolution, tf_resolution=128, fov=30.0, nearfar=(0.1, 100.0)):
         ''' Initializes Volume Raycaster. Make sure to .set_volume() and .set_tf_tex() after initialization
 
         Args:
-            resolution (2-tuple of int, optional): Resolution of the rendering (w,h). Defaults to (240, 240).
+            volume_resolution (3-tuple of int): Resolution of the volume data (w,h,d)
+            render_resolution (2-tuple of int): Resolution of the rendering (w,h)
+            tf_resolution (int): Resolution of the transfer function texture
             fov (float, optional): Field of view of the camera in degrees. Defaults to 60.0.
             nearfar (2-tuple of float, optional): Near and far plane distance used for perspective projection. Defaults to (0.1, 100.0).
         '''
-        self.resolution = resolution
-        self.aspect = resolution[0] / resolution[1]
+        self.resolution = render_resolution
+        self.aspect = render_resolution[0] / render_resolution[1]
         self.fov_deg = fov
         self.fov_rad = np.radians(fov)
         self.near, self.far = nearfar
         # Taichi Fields
-        self.volume = ti.field(ti.f32, shape=(184, 256, 170))
-        self.tf_tex = ti.Vector.field(4, dtype=ti.f32, shape=(128,))
-        self.render = ti.Vector.field(3, dtype=ti.f32, shape=resolution)
-        self.opacity = ti.field(ti.f32, shape=resolution)
+        self.volume = ti.field(ti.f32, shape=volume_resolution)
+        self.tf_tex = ti.Vector.field(4, dtype=ti.f32, shape=tf_resolution)
+        self.render = ti.Vector.field(3, dtype=ti.f32, shape=render_resolution)
+        self.opacity = ti.field(ti.f32, shape=render_resolution)
+        self.ambient = 0.4
+        self.diffuse = 0.8
+        self.specular = 0.3
+        self.shininess = 32.0
+        self.light_color = tl.vec3(1.0)
 
     def set_volume(self, volume): self.volume.from_numpy(volume)
     def set_tf_tex(self, tf_tex): self.tf_tex.from_numpy(tf_tex)
@@ -104,7 +110,7 @@ class VolumeRaycaster():
         return (near_pos - orig).normalized()
 
     @ti.func
-    def sample_volume_trilinear(self, pos):
+    def sample_volume_trilinear(self, x: float, y:float, z:float):
         ''' Samples volume data at `pos` and trilinearly interpolates the value
 
         Args:
@@ -113,7 +119,7 @@ class VolumeRaycaster():
         Returns:
             float: Sampled interpolated intensity
         '''
-        pos = (0.5*pos) + 0.5
+        pos = ((0.5*tl.vec3(x, y, z)) + 0.5) * ti.static(tl.vec3(*self.volume.shape) - 1 -1e-6)
         x_low, x_high, x_frac = low_high_frac(pos.x)
         y_low, y_high, y_frac = low_high_frac(pos.y)
         z_low, z_high, z_frac = low_high_frac(pos.z)
@@ -136,16 +142,24 @@ class VolumeRaycaster():
         return tl.mix(xy_val_z_low, xy_val_z_high, z_frac)
 
     @ti.func
+    def get_volume_normal(self, pos):
+        delta = 1e-3
+        dx = self.sample_volume_trilinear(pos.x + delta, pos.y, pos.z) - self.sample_volume_trilinear(pos.x - delta, pos.y, pos.z)
+        dy = self.sample_volume_trilinear(pos.x, pos.y + delta, pos.z) - self.sample_volume_trilinear(pos.x, pos.y - delta, pos.z)
+        dz = self.sample_volume_trilinear(pos.x, pos.y, pos.z + delta) - self.sample_volume_trilinear(pos.x, pos.y, pos.z - delta)
+        return tl.vec3(dx, dy, dz).normalized()
+
+    @ti.func
     def apply_transfer_function(self, intensity: float):
         ''' Applies a 1D transfer function to a given intensity value
 
         Args:
-            intensity (float): Intensity
+            intensity (float): Intensity in [0,1]
 
         Returns:
             tl.vec4: Color and opacity for given `intensity`
         '''
-        length = ti.static(float(self.tf_tex.shape[0]))
+        length = ti.static(float(self.tf_tex.shape[0] - 1) - 1e-4)
         low, high, frac = low_high_frac(intensity * length)
         return tl.mix(self.tf_tex[low], self.tf_tex[high], frac)
 
@@ -163,6 +177,7 @@ class VolumeRaycaster():
         max_y = ti.static(float(self.render.shape[1]))
         vol_diag = ti.static((tl.vec3(*self.volume.shape) - tl.vec3(1.0)).norm())
         look_from = tl.vec3(cam_pos_x, cam_pos_y, cam_pos_z)
+        light_pos = look_from + tl.vec3(0.0, 1.0, 0.0)
         view_dir = (-look_from).normalized()
         right_dir = tl.cross(view_dir, ti.static(tl.vec3(0.0, 1.0, 0.0))).normalized()
         up_dir    = tl.cross(right_dir, view_dir).normalized()
@@ -177,17 +192,25 @@ class VolumeRaycaster():
             if hit: # Shoot the ray
                 ray_len = (tmax - tmin)
                 n_samples = ti.ceil(sampling_rate * ray_len * vol_diag) # Number of samples according to https://osf.io/u9qnz
-                t_inc = ray_len / n_samples # Increment for steps along vd
-                t = tmin + 0.5 * t_inc # Start of ray is look_from + t * vd
+                tmin += 0.5 * ray_len / n_samples # Offset tmin as t_start
                 color = tl.vec4(0.0) # Color to be composited
                 cnt = 0
                 while cnt < n_samples and color.w < 0.99: # Early ray termination
-                    pos = look_from + t * vd # Current pos at this step
-                    intensity = self.sample_volume_trilinear(pos)     # Sample intensity
-                    s_color = self.apply_transfer_function(intensity) # Sample Color and opacity
-                    s_color = tl.vec4(s_color.xyz * s_color.w, s_color.w) # pre-multiply alpha
-                    color += (1.0 - color.w) * s_color  # Composite color
-                    t += t_inc
+                    pos = look_from + tl.mix(tmin, tmax, float(cnt)/float(n_samples)) * vd # Current Pos
+                    intensity = self.sample_volume_trilinear(pos.x, pos.y, pos.z)
+                    sample_color = self.apply_transfer_function(intensity)
+                    if sample_color.w > 1e-3:
+                        normal = self.get_volume_normal(pos)
+                        light_dir = (pos - light_pos).normalized() # Direction to light source
+                        n_dot_l = max(normal.dot(light_dir), 0.0)
+                        diffuse = self.diffuse * n_dot_l
+                        r = tl.reflect(light_dir, normal) # Direction of reflected light
+                        r_dot_v = max(r.dot(-vd), 0.0)
+                        specular = self.specular * pow(r_dot_v, self.shininess)
+                        shaded_color = tl.vec4((self.ambient + diffuse + specular) * sample_color.xyz * sample_color.w * self.light_color, sample_color.w)
+                        color += (1.0 - color.w) * shaded_color # Composite color
+                        # color = tl.vec4(normal, 1.0)
+                        # break
                     cnt += 1
                 # Save result to global buffers
                 self.render[i, j] = color.xyz
@@ -196,24 +219,70 @@ class VolumeRaycaster():
                 self.render[i, j] = tl.vec3(0.0)
                 self.opacity[i,j] = 0.0
 
+    @ti.kernel
+    def draw_entry_points(self, cam_pos_x: float, cam_pos_y: float, cam_pos_z: float):
+        max_x = ti.static(float(self.render.shape[0]))
+        max_y = ti.static(float(self.render.shape[1]))
+        vol_diag = ti.static((tl.vec3(*self.volume.shape) - tl.vec3(1.0)).norm())
+        look_from = tl.vec3(cam_pos_x, cam_pos_y, cam_pos_z)
+        view_dir = (-look_from).normalized()
+        right_dir = tl.cross(view_dir, ti.static(tl.vec3(0.0, 1.0, 0.0))).normalized()
+        up_dir    = tl.cross(right_dir, view_dir).normalized()
+
+        bb_bl = ti.static(tl.vec3(-1.0, -1.0, -1.0)) # Bounding Box bottom left
+        bb_tr = ti.static(tl.vec3( 1.0,  1.0,  1.0)) # Bounding Box bottom right
+        for i, j in self.render: # For all pixels
+            x = (float(i) + 0.5) / max_x # Get pixel centers in range (0,1)
+            y = (float(j) + 0.5) / max_y #
+            vd = self.get_ray_direction(look_from, view_dir, x, y) # Get exact view direction to this pixel
+            tmin, tmax, hit = get_entry_exit_points(look_from, vd, bb_bl, bb_tr) # distance along vd till volume entry and exit, hit bool
+            if hit: # Shoot the ray
+                self.render[i, j] = tl.vec3(look_from + tmin * vd)
+            else:
+                self.render[i, j] = tl.vec3(0.0)
+
+def in_circles(i, y=0.7, dist=2.5):
+    x = np.cos(i) * dist
+    z = np.sin(i) * dist
+    return x, y, z
+
+def rotate_camera(gui):
+    gui.get_event()
+    if gui.is_pressed('d'):
+        return 0.1
+    elif gui.is_pressed('a'):
+        return -0.1
+    else:
+        return 0.0
+
 
 if __name__ == '__main__':
-    ti.init(arch=ti.cpu, debug=True, advanced_optimization=False, excepthook=True, log_level=ti.TRACE)
-    window = ti.GUI("Volume Raycaster", res=(400, 400), fast_gui=False)
-    vr = VolumeRaycaster(resolution=(400,400))
-    print('GUI Resolution:', window.res, '  Framebuffer shape: ', vr.render.shape)
-    tfgen = TFGenerator()
-    tf = tex_from_pts(tfgen.generate(), 128).numpy().astype(np.float32)
-    vol = (load_head_data() / float(np.iinfo(np.uint16).max)).astype(np.float32)
+    RESOLUTION = (300, 300)
+    TF_RESOLUTION = 128
+    # ti.init(arch=ti.cpu, debug=True, excepthook=True, log_level=ti.TRACE)
+    ti.init(arch=ti.gpu)
+    gui = ti.GUI("Volume Raycaster", res=RESOLUTION, fast_gui=True)
+    gui.slider('Shininess', 0, 1024)
+
+
+    tf = tex_from_pts(np.array([[0.0000, 0.0000, 0.0000, 0.0000, 0.0000],
+                                [0.3308, 0.1512, 0.6418, 0.8293, 0.0000],
+                                [0.3508, 0.1512, 0.6418, 0.8293, 0.5465],
+                                [0.3714, 0.1512, 0.6418, 0.8293, 0.7660],
+                                [0.4297, 0.1512, 0.6418, 0.8293, 0.7660],
+                                [0.4504, 0.1512, 0.6418, 0.8293, 0.5465],
+                                [0.4704, 0.1512, 0.6418, 0.8293, 0.0000],
+                                [1.0000, 0.0000, 0.0000, 0.0000, 0.0000]]), TF_RESOLUTION).permute(1, 0).contiguous().numpy().astype(np.float32)
+    vol_ds = TorchDataset('/run/media/dome/Data/data/torchvtk/CQ500')
+    vol = vol_ds[0]['vol'].permute(2, 0, 1).contiguous().numpy().astype(np.float32)
+    # vol = (load_head_data() / float(np.iinfo(np.uint16).max)).astype(np.float32)
+    vr = VolumeRaycaster(volume_resolution=vol.shape, render_resolution=RESOLUTION, tf_resolution=TF_RESOLUTION)
     vr.set_volume(vol)
     vr.set_tf_tex(tf)
-    while window.running:
-        vr.raycast(0.0, 0.1, 3.0, 2.0)
-        render_np = vr.render.to_numpy()
-        # plt.imshow(render_np)
-        # plt.show()
-        print(vr.render.dtype, vr.render.shape, vr.render.n, vr.render.m)
-        print(render_np.dtype, render_np.shape, render_np.min(), render_np.max())
-        window.set_image(vr.render)
-        window.show()
-        break
+    t = np.pi * 1.5
+    while gui.running:
+        vr.raycast(*in_circles(t), 2.0)
+        # vr.draw_entry_points(0.0, 0.1, 3.0)
+        t += rotate_camera(gui)
+        gui.set_image(vr.render)
+        gui.show()
